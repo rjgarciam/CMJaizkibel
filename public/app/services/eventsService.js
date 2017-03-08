fbe8e819b2fb21667fea434a3d58a76ddfb1cb79
Ricardo Garcia <ricardo@rjgarcia.es>
2017-03-08 11:28:45 +0100

    MaxNumberEvents
    Change from MaxDate to number (10 events)

--- public/app/services/eventsService.js
+++ public/app/services/eventsService.js
@@ -8,13 +8,13 @@ angular.module('EventsService', [])
   // get all events from given calendar
   EventsFactory.mainCal = function(calendarURL,APIKey) {
     var today = new Date();
-    var maxDay = new Date(new Date().setDate(new Date().getDate()+7));
+    //var maxDay = new Date(new Date().setDate(new Date().getDate()+7));
     var config = { params:{
                 key:  APIKey,
                 timeMin: today,
                 singleEvents: true,
                 orderBy: 'startTime',
-                timeMax: maxDay,
+                maxResults: 10,
               }};
     return $http.get('https://www.googleapis.com/calendar/v3/calendars/' + calendarURL + '/events', config);
   };
