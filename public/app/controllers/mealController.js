959403c2f755210e537195036d003858cd295e4e
Ricardo Garcia <ricardo@rjgarcia.es>
2017-09-14 10:50:25 +0200

    Modify meals list

--- public/app/controllers/mealController.js
+++ public/app/controllers/mealController.js
@@ -29,7 +29,7 @@ angular.module('mealCtrl',[])
     /*  02 */ 'Como a las 15:15',
     /*  03 */ 'No ceno',
     /*  04 */ 'Bocadillos en la cena a las 20:00',
-    /*  05 */ 'Ceno a las 22:00',
+    /*  05 */ 'Cena tarde ENFERMERÍA',
     /*  06 */ 'No desayuno',
     /*  07 */ 'Desayuno 7:15',
     /*  08 */ 'Bocadillos a las 11:30',
