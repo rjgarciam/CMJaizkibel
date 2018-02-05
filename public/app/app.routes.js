3def2c792622f99250b50c3e4dd0cf76ce7f5ae3
Ricardo Garcia <ricardo@rjgarcia.es>
2018-02-06 00:10:02 +0100

    CS18

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -152,6 +152,17 @@ angular.module('app.routes', ['ngRoute'])
       }
     })
 
+    // Meals section
+    .when('/cs18', {
+      templateUrl : 'app/views/pages/cs18.html',
+      controller  : 'MealController',
+          controllerAs: 'meals',
+      data: {
+        reqPermissions: 'isLogged',
+        redirect: '/login',
+      }
+    })
+
 
     .otherwise({ redirectTo: '/meals'})
 
