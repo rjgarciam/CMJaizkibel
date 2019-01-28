b308f1669dcc655ad1e8320014552f9df997a67e
Ricardo <ricardo@rjgarcia.es>
2019-01-28 12:11:55 +0100

    new Auth

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,5 +1,5 @@
 var config     = require('./config');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var GClientID = process.env.GCLIENTID || config.googleAuth.clientID;
 var GClientSecret = process.env.GCLIENTSECRET || config.googleAuth.clientSecret;
 var GCallback = process.env.GCALLBACK || config.googleAuth.callbackURL;
@@ -14,7 +14,8 @@ module.exports = function(passport){
 	passport.use(new GoogleStrategy({
 	    clientID: GClientID,
 	    clientSecret: GClientSecret,
-	    callbackURL: GCallback
+	    callbackURL: GCallback,
+	    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
 	  },
 	  function(accessToken, refreshToken, profile, done) {
 	    return done(null, profile.emails[0].value);
